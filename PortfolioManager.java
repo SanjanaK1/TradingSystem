3a13184c097e51ae23c14b48be318cebeed06c5a
SanjanaK1 <skasarla@bu.edu>
2023-04-18 11:47:13 -0400

    added classes

--- PortfolioManager.java
+++ PortfolioManager.java
@@ -6,6 +6,13 @@ import java.util.ArrayList;
 public class PortfolioManager {
     ArrayList<Customer> customerList = new ArrayList<Customer>();
 
+
+
+    public boolean approveCustomer()
+    {
+        return false;
+    }
+
     //factory pattern
     //she wants to see composite and decorator
 
