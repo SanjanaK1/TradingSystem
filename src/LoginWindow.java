8090797b88165f2447052e375603fec2925dccf0
Mark Yang <mdyang@bu.edu>
2023-04-20 11:59:22 -0400

    Update things?

--- src/LoginWindow.java
+++ src/LoginWindow.java
@@ -5,6 +5,22 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class LoginWindow extends JFrame implements ActionListener {
+    public LoginWindow(){
+        initComponents();
+    }
+    private void initComponents(){
+        JButton loginInUser = new JButton("Log In as User");//creating instance of JButton
+        JButton loginInPM = new JButton("Log In as Portfolio Manager");
+        loginInUser.setBounds(250,300,200, 40);//x axis, y axis, width, height
+        add(loginInUser);
+
+        loginInPM.setBounds(230,350,250, 40);//x axis, y axis, width, height
+        add(loginInPM);
+
+        setSize(700,700);
+        setLayout(null);//using no layout managers
+        setVisible(true);//making the frame visible
+    }
 
     public void createWindow(){
         JFrame f = new JFrame();
--- src/TradingSystem.java
+++ src/TradingSystem.java
@@ -2,13 +2,15 @@ package src;
 
 import src.LoginWindow;
 
+import src.LoginWindow;
+
 public class TradingSystem {
 
     public static void main(String args[]){
 
         LoginWindow login = new LoginWindow();
 
-        login.createWindow();
+        login.setVisible(true);
     }
 
 
