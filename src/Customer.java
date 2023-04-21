26ad0e5e193da128789b56a5fd969b7fbfef0bf9
sataalalii <sataalalii@proton.me>
2023-04-21 13:03:55 -0400

    Updated Portfolio, Stock, StockMarket, and Customer classes to handle selling and buying of stocks.

--- src/Customer.java
+++ src/Customer.java
@@ -1,8 +1,64 @@
-package src;
+package src.src;
 
 public class Customer {
 
-    int approved = 0; //0 = not approved
-    int money;
-    Portfolio portfolio = new Portfolio();
+    private boolean approved = false ; //0 = not approved
+    private double money;
+    private Portfolio portfolio = new Portfolio(null);
+
+
+    public boolean isApproved() {
+        return approved;
+    }
+
+    public void setApproved(boolean approved) {
+        this.approved = approved;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+
+    public double getNetValue() {
+        return this.getMoney() + this.getPortfolio().getStockListValue();
+    }
+
+    public boolean buyStock(Stock s, int quantityBought) {
+        boolean bought = false;
+        double stockCost = quantityBought * s.getCurrentPrice();
+        if (s != null && StockMarket.isStockInMarket(s) && this.money >= stockCost) {
+            //TODO: process purchase with customer money.
+            boolean isSuccessfulPurchase = StockMarket.purchase(s, quantityBought);
+            if (isSuccessfulPurchase) {
+                portfolio.addStock(s, quantityBought);
+                setMoney(this.money - stockCost);
+                bought = true;
+            }
+        }
+
+        return bought;
+    }
+
+    public boolean sellStock(Stock s, int quantitySold) {
+        boolean sold = false;
+        if (s != null && portfolio.getStockQuantity(s) > 0) {
+            portfolio.removeStock(s, quantitySold);
+            double netChange = s.getNetChange() * quantitySold;
+            setMoney(netChange);
+            sold = true;
+        }
+        return sold;
+    }
 }
--- src/Stock.java
+++ src/Stock.java
@@ -1,8 +1,36 @@
-package src;
+package src.src;
 
-public class Stock {
+public class Stock implements Tradeable{
 
-    double price;
-    String name;
-    boolean tradable;
+    private double boughtPrice;
+    private double currentPrice;
+    private String name;
+
+    public double getBoughtPrice() {
+        return boughtPrice;
+    }
+
+    public void setBoughtPrice(double boughtPrice) {
+        this.boughtPrice = boughtPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public double getNetChange() {
+        return currentPrice - boughtPrice;
+    }
 }
