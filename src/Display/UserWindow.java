274db6c880f92d036e9abadfe8b25db1473d0d5e
Mark Yang <mdyang@bu.edu>
2023-04-25 11:47:30 -0400

    Refactor of display classes

---
+++ src/Display/UserWindow.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UserWindow extends JFrame {
+    public UserWindow(Person p){
+        //TODO
+        initComponents();
+    }
+
+    private void initComponents() {
+    }
+}
