894d0577601a010c7565fc0eee3ed44962cb79a3
Mark Yang <mdyang@bu.edu>
2023-05-02 20:19:08 -0400

    Started facade of whats needed. Fixed project structure for the .iml again

---
+++ src/DisplayFacade.java
@@ -0,0 +1,13 @@
+public class DisplayFacade {
+
+    public String[] getTableHeader(){
+        return new String[]{"Stock Name", "Value ($)", "Number Owned"};
+    }
+    public String[][] getTableData(){
+        return null;
+    }
+
+    public String getUserName(){
+        return null;
+    }
+}
