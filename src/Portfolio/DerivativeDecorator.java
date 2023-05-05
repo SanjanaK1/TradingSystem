78637597f75db37328db01f7bf3f2fea57d65669
Mark Yang <mdyang@bu.edu>
2023-05-05 15:54:04 -0400

    Made DerivativeDecorator abstract

--- src/Portfolio/DerivativeDecorator.java
+++ src/Portfolio/DerivativeDecorator.java
@@ -1,4 +1,4 @@
-public class DerivativeDecorator extends PortfolioDecorator{
+public abstract class DerivativeDecorator extends PortfolioDecorator {
 
 
     public DerivativeDecorator(Portfolio portfolio) {
@@ -8,36 +8,6 @@ public class DerivativeDecorator extends PortfolioDecorator{
     @Override
     public String makePortfolio() {
         return super.makePortfolio() + "\n" + addDerivative();
-    }
-
-    @Override
-    public void addStock(Stock s, int quantityBought) {
-
-    }
-
-    @Override
-    public void removeStock(Stock s, int quantitySold) {
-
-    }
-
-    @Override
-    public int getStockQuantity(Stock s) {
-        return 0;
-    }
-
-    @Override
-    public double getStockListValue() {
-        return 0;
-    }
-
-    @Override
-    public int getStockListCount() {
-        return 0;
-    }
-
-    @Override
-    public Stock[] getStockArray() {
-        return new Stock[0];
     }
 
     private String addDerivative() {
