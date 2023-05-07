9ffc22d6fee50245f2f656a636fb4643a6cfee0a
Mark Yang <mdyang@bu.edu>
2023-05-07 18:22:29 -0400

    Updated DisplayFacade and ManagerWindow to show stock values

--- src/Display/StockManagerTableModel.java
+++ src/Display/StockManagerTableModel.java
@@ -1,8 +1,12 @@
 import javax.swing.table.DefaultTableModel;
+import java.util.List;
 
 public class StockManagerTableModel extends DefaultTableModel {
     public StockManagerTableModel(String[][] cells, String[] headers) {
         super(cells, headers);
+    }
+    public StockManagerTableModel(List<String[]> cells, String[] headers) {
+        this(cells.stream().toArray(String[][]::new), headers);
     }
 
     public void newStock() {
